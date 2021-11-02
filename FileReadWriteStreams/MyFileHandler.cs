cf55df32bc8a7c94f5721b81722ae2c65e33eb31
pethaf <pethaf1@gmail.com>
2021-11-02 11:06:45 +0100

    Add project files.

---
+++ FileReadWriteStreams/MyFileHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReadWriteStreams
+{
+    class MyFileHandler
+    {
+        public void ReadAllContent(string fileName)
+        {
+            using(StreamReader sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+        }
+        public void WriteContentToFile(string FileName, string contentToWrite)
+        {
+            using(StreamWriter sw = new StreamWriter(FileName))
+            {
+
+                sw.WriteLine(contentToWrite);
+            }
+        }
+        public void UpperCaseContent(string filename1, string filename2)
+        {
+            using(StreamReader sr = new StreamReader(filename1))
+            {
+                using(StreamWriter sw = new StreamWriter(filename2))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        sw.WriteLine(sr.ReadLine().ToUpper());
+                    }
+                }
+            }
+        }
+        public void ReadLatinEncodedContent(string fileName)
+        {
+            using(StreamReader sw = new StreamReader(fileName,Encoding.Latin1))
+            {
+                while (!sw.EndOfStream)
+                {
+                    var theChar = sw.Read();
+                    var theString = $"{theChar,-25} {(char)theChar}";
+                    Console.WriteLine(theString);
+                }
+            }
+        }
+    }
+}
---
+++ FileReadWriteStreams/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileReadWriteStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFileHandler mf = new MyFileHandler();
+            mf.ReadAllContent("testfil1.txt");
+            mf.WriteContentToFile("testfil2", "Jag är en fisk");
+            mf.UpperCaseContent("rock.txt", "rockuppercase.txt");
+            mf.ReadLatinEncodedContent("latin1.txt");
+                }
+
+    }
+}
